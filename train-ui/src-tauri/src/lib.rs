378d5afa23d0540e0d29c75ed479930c53ad37b7
Ben Sutherland <11659668+BlenderSleuth@users.noreply.github.com>
2024-09-13 12:12:47 +1000

    Added basic rust command run from UI button.

--- train-ui/src-tauri/src/lib.rs
+++ train-ui/src-tauri/src/lib.rs
@@ -1,6 +1,13 @@
+#[tauri::command]
+fn my_custom_command(button: &str) {
+    println!("{button} was pressed!");
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // .plugin(tauri_plugin_fs::init())
+        .invoke_handler(tauri::generate_handler![my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- train-ui/src/main.ts
+++ train-ui/src/main.ts
@@ -1,2 +1,13 @@
-import '../fonts/inter.css'
-import './style.css'
+import '../fonts/inter.css';
+import './style.css';
+
+import { invoke } from '@tauri-apps/api/core';
+
+const buttons = document.getElementsByTagName('button');
+
+for (let button of buttons) {
+  button.addEventListener('click', async (event) => {
+    let element = event.target as HTMLButtonElement;
+    await invoke('my_custom_command', { button: element.name });
+  });
+}
